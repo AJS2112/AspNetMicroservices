29ea96061e01ffb000bd5aa40eb773003c508aaa
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-05-07 08:05:57 -0300

    Creating Catalog.API DataContext

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration["DatabaseSettings:ConnectionString"]);
+
+            Products = database.GetCollection<Product>(configuration["DatabaseSettings:CollectionName"]);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get;  }
+    }
+}
