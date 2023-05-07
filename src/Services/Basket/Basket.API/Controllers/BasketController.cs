4d23ba43a3cfc6eb4a82ab2d51328e8d00941bce
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-05-07 18:03:50 -0300

    Consume Discount.Grpc Microservice in Basket.API Microservice

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,13 @@ namespace Basket.API.Controllers
     [ApiController]
     public class BasketController : ControllerBase
     {
-        private IBasketRepository _repository;
+        private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService)
         {
             _repository = repository;
+            _discountGrpcService = discountGrpcService;
         }
 
         [HttpGet("{userName}", Name= "GetBasket")]
@@ -28,6 +31,12 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            foreach(var item in basket.Items)
+            {
+                var coupon = await _discountGrpcService.GetDiscount(item.ProductId);
+                item.Price -= coupon.Amount;
+            }
+
             return Ok(_repository.UpdateBasket(basket));    
         }
 
---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,20 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _client;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<CouponModel> GetDiscount(string producName)
+        {
+            var discountRequest = new GetDiscountRequest {  ProductName = producName };
+            return await _client.GetDiscountAsync(discountRequest);
+        }
+    }
+}
