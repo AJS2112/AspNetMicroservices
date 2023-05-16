a9a85ebae2785ec91ea3384ed3ee4b26e2deab80
Antonio Jose Sucre Gonzalez <antonio.gonzalez@efrete.com.br>
2023-05-16 08:25:06 -0300

    Ordering Application Layer - DeleteOrderCommand

---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationException : ApplicationException
+    {
+        public ValidationException()
+            : base("One or more validation failures have ocurred")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationException(IEnumerable<ValidationFailure> validationFailures)
+            : this()
+        {
+            Errors = validationFailures
+                .GroupBy(e => e.PropertyName, e => e.ErrorMessage)
+                .ToDictionary(failureGroup => failureGroup.Key, failureGroup => failureGroup.ToArray());    
+        }
+
+        public IDictionary<string, string[]> Errors { get; set; }
+
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleOrderCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Exceptions;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    internal class DeleOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleOrderCommandHandler> _logger;
+
+        public DeleOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<DeleOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete = await _orderRepository.GetByIdAsync(request.Id);
+            if (orderToDelete == null)
+            {
+                _logger.LogError("Order not exists on the database");
+                throw new NotFoundException(nameof(Order), request.Id);
+            }
+
+            await _orderRepository.DeleteAsync(orderToDelete);
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted");
+        }
+    }
+}
